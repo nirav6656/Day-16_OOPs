bf7f49260b0942c5f4e254f8143d646b1949d71d
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-03-19 22:26:45 -0400

    Trying to install different packages.

---
+++ main.py
@@ -0,0 +1,12 @@
+from circle_fitting_3d import Circle3D
+
+
+# circle = Circle3D()
+
+# Add columns and data to the table
+# table.add_column("City name", ["Toronto", "London", "Ahmedabad", "Surat"])
+# table.add_column("Area", [1295, 5905, 112, 1357, 2058])
+circle_3d = Circle3D([[3, 0, 0], [0, 3, 0], [0, 0, 3]])
+
+# Print the table
+print(circle_3d)
